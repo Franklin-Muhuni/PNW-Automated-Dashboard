0ad44ef56d8387c5d8d32eacbdafad43697d433d
Franklin Muhuni <70990257+Franklin-Muhuni@users.noreply.github.com>
2023-05-10 17:15:17 -0400

    Update PnWDB Schema.sql

--- PnWDB Schema.sql
+++ PnWDB Schema.sql
@@ -1,11 +1,9 @@
-
 USE PnWDB;
 GO
 
 CREATE TABLE [alliances] (
   [alliance_id] SMALLINT PRIMARY KEY NOT NULL,
   [alliance_name] VARCHAR(100) NOT NULL,
-  [members] SMALLINT,
   [founded] DATETIME NOT NULL,
   [color_bloc] VARCHAR(100) NOT NULL,
   [alliance_score] FLOAT NOT NULL
@@ -21,6 +19,7 @@ CREATE TABLE [nations] (
   [color_bloc] VARCHAR(100) NOT NULL,
   [nation_score] FLOAT NOT NULL,
   [alliance_id] SMALLINT NULL,
+  [alliance_position] VARCHAR(50) NOT NULL,
   [population] INT NOT NULL,
   [gdp] BIGINT NOT NULL,
   [gni] BIGINT NOT NULL,
@@ -33,17 +32,15 @@ CREATE TABLE [nations] (
   [ships] SMALLINT NOT NULL,
   [missiles] SMALLINT NOT NULL,
   [nukes] SMALLINT NOT NULL,
-  [wars_won] SMALLINT NOT NULL,
-  [wars_lost] SMALLINT NOT NULL,
   [last_active] DATETIME NOT NULL
 )
 GO
 
 CREATE TABLE [treaties] (
-  [treaty_id] INT PRIMARY KEY NOT NULL,
+  [id] INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  [treaty_id] INT NOT NULL,
   [treaty_type] VARCHAR(50) NOT NULL,
-  [sending_alliance_id] SMALLINT NOT NULL,
-  [receiving_alliance_id] SMALLINT NOT NULL,
+  [alliance_id] SMALLINT NOT NULL,
   [date_signed] DATETIME NOT NULL,
   [turns_left] SMALLINT NOT NULL
 )
@@ -63,10 +60,7 @@ GO
 ALTER TABLE [nations] ADD FOREIGN KEY ([alliance_id]) REFERENCES [alliances] ([alliance_id])
 GO
 
-ALTER TABLE [treaties] ADD FOREIGN KEY ([sending_alliance_id]) REFERENCES [alliances] ([alliance_id])
-GO
-
-ALTER TABLE [treaties] ADD FOREIGN KEY ([receiving_alliance_id]) REFERENCES [alliances] ([alliance_id])
+ALTER TABLE [treaties] ADD FOREIGN KEY ([alliance_id]) REFERENCES [alliances] ([alliance_id])
 GO
 
 ALTER TABLE [cities] ADD FOREIGN KEY ([nation_id]) REFERENCES [nations] ([nation_id])
